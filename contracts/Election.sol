abe1c6b94ba1a0387e9f728d6515a28ab00029ba
Tom Biskup <tom.biskup@rwth-aachen.de>
2018-06-17 10:11:41 -0700

    Remove build folder

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -26,10 +26,10 @@ contract Election {
         // Authorize certain people
         address nathanAuth = 0xF415156fA2540e1488CE57B7CC4f751642f7f90c;
         address keerthanaAuth = 0x5629cB04722435AE2C85e37a1Fd61f0AF6EA4dC0;
-        address tomAuth = 0x8586c212FDC0bf87dd6Fd90fFec35b0c29301872;
+        address tomAuth = 0xd82d10e270770e4ec46483a52577FE85e35472A9;
         address nathanNotAuth = 0x8586c212FDC0bf87dd6Fd90fFec35b0c29301872;
         address keerthanaNotAuth = 0x3CD7e4491244176E70239C92353F3f7f43146C8D;
-        address tomNotAuth = 0xD482940Ba6B2429b38E634B50A954EA7803011D0;
+        address tomNotAuth = 0x27A0a1541976E203EDef3743B5F237e4da263f59;
         authorized[nathanAuth] = true;
         authorized[keerthanaAuth] = true;
         authorized[tomAuth] = true;
